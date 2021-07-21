b5c5852abc86879d0111a6bb52af8225b737fa1a
Pedro Cassa Dias <pcassa@id.uff.br>
2021-07-21 12:16:42 -0300

    Menu de Salas Inicial

--- lib/screens/main-menu.dart
+++ lib/screens/main-menu.dart
@@ -54,178 +54,181 @@ class _MainMenuState extends State<MainMenu> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      resizeToAvoidBottomPadding: false,
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/Background.png'),
-                fit: BoxFit.cover,
-                colorFilter:
-                    ColorFilter.mode(Colors.black45, BlendMode.darken))),
-        child: Column(
-          children: <Widget>[
-            Image.asset(
-              'assets/images/Logo.png',
-              height: 400.0,
-            ),
-            screenController == 'Home'
-                ? SizedBox(
-                    width: 200,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          screenController = 'Login';
-                        });
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.orange,
-                      ),
-                      child: Text(
-                        'Iniciar Jogo',
-                        style: new TextStyle(
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
-                  )
-                : Text(''),
-            screenController == 'Login' || screenController == 'Cadastrar'
-                ? SizedBox(
-                    width: 250,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {});
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                      ),
-                      child: TextFormField(
-                        validator: (val) => val.length < 4
-                            ? 'Nome de usuario muito curto.'
-                            : null,
-                        onChanged: (value) {
-                          if (value.length > 0) {
-                            playerName = value;
-                          }
-                        },
-                        decoration: InputDecoration(
-                            border: InputBorder.none,
-                            labelText: 'Digite seu usuario'),
-                      ),
-                    ),
-                  )
-                : Text(''),
-            screenController == 'Login' || screenController == 'Cadastrar'
-                ? Padding(
-                    padding: const EdgeInsets.only(
-                      left: 40,
-                      top: 20,
-                      right: 40,
-                      bottom: 20,
-                    ),
-                    child: SizedBox(
-                      width: 250,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          setState(() {});
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.white,
-                        ),
-                        child: TextFormField(
-                          obscureText: _obscureText,
-                          validator: (val) =>
-                              val.length < 4 ? 'Senha muito curta.' : null,
-                          onChanged: (value) {
-                            if (value.length > 0) {
-                              playerPassword = value;
-                            }
+        resizeToAvoidBottomInset: true,
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/Background.png'),
+                    fit: BoxFit.cover,
+                    colorFilter:
+                        ColorFilter.mode(Colors.black45, BlendMode.darken))),
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/Logo.png',
+                  height: 350.0,
+                ),
+                screenController == 'Home'
+                    ? SizedBox(
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              screenController = 'Login';
+                            });
                           },
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              labelText: 'Digite sua senha'),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.orange,
+                          ),
+                          child: Text(
+                            'Iniciar Jogo',
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                            ),
+                          ),
                         ),
-                      ),
-                    ))
-                : Text(''),
-            screenController == 'Login'
-                ? SizedBox(
-                    width: 200,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          // var db = FirebaseFirestore.instance;
-                          // db.collection("usuarios").add({
-                          //   "nome": playerName,
-                          // });
-                        });
-                        Navigator.pushNamed(context, router.userMenuPage);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.deepPurpleAccent,
-                      ),
-                      child: Text(
-                        'Login',
-                        style: new TextStyle(
-                          fontSize: 20.0,
+                      )
+                    : Text(''),
+                screenController == 'Login' || screenController == 'Cadastrar'
+                    ? SizedBox(
+                        width: 250,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {});
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.white,
+                          ),
+                          child: TextFormField(
+                            validator: (val) => val.length < 4
+                                ? 'Nome de usuario muito curto.'
+                                : null,
+                            onChanged: (value) {
+                              if (value.length > 0) {
+                                playerName = value;
+                              }
+                            },
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                labelText: 'Digite seu usuario'),
+                          ),
                         ),
-                      ),
-                    ),
-                  )
-                : Text(''),
-            screenController == 'Home' || screenController == 'Cadastrar'
-                ? SizedBox(
-                    width: 200,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          if (screenController == 'Cadastrar') {
-                            signInNewUser();
-                          } else {
-                            screenController = 'Cadastrar';
-                          }
-                          // var db = FirebaseFirestore.instance;
-                          // db.collection("usuarios").add({
-                          //   "nome": playerName,
-                          // });
-                        });
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.green,
-                      ),
-                      child: Text(
-                        'Cadastrar',
-                        style: new TextStyle(
-                          fontSize: 20.0,
+                      )
+                    : Text(''),
+                screenController == 'Login' || screenController == 'Cadastrar'
+                    ? Padding(
+                        padding: const EdgeInsets.only(
+                          left: 40,
+                          top: 20,
+                          right: 40,
+                          bottom: 20,
                         ),
-                      ),
-                    ),
-                  )
-                : Text(''),
-            screenController != 'Home'
-                ? SizedBox(
-                    width: 200,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          screenController = 'Home';
-                        });
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.redAccent,
-                      ),
-                      child: Text(
-                        'Retornar',
-                        style: new TextStyle(
-                          fontSize: 20.0,
+                        child: SizedBox(
+                          width: 250,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {});
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                            ),
+                            child: TextFormField(
+                              obscureText: _obscureText,
+                              validator: (val) =>
+                                  val.length < 4 ? 'Senha muito curta.' : null,
+                              onChanged: (value) {
+                                if (value.length > 0) {
+                                  playerPassword = value;
+                                }
+                              },
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  labelText: 'Digite sua senha'),
+                            ),
+                          ),
+                        ))
+                    : Text(''),
+                screenController == 'Login'
+                    ? SizedBox(
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              // var db = FirebaseFirestore.instance;
+                              // db.collection("usuarios").add({
+                              //   "nome": playerName,
+                              // });
+                            });
+                            Navigator.pushNamed(context, router.userMenuPage);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.deepPurpleAccent,
+                          ),
+                          child: Text(
+                            'Login',
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  )
-                : Text(''),
-          ],
-        ),
-      ),
-    );
+                      )
+                    : Text(''),
+                screenController == 'Home' || screenController == 'Cadastrar'
+                    ? SizedBox(
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              if (screenController == 'Cadastrar') {
+                                signInNewUser();
+                              } else {
+                                screenController = 'Cadastrar';
+                              }
+                              // var db = FirebaseFirestore.instance;
+                              // db.collection("usuarios").add({
+                              //   "nome": playerName,
+                              // });
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.green,
+                          ),
+                          child: Text(
+                            'Cadastrar',
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      )
+                    : Text(''),
+                screenController != 'Home'
+                    ? SizedBox(
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              screenController = 'Home';
+                            });
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.redAccent,
+                          ),
+                          child: Text(
+                            'Retornar',
+                            style: new TextStyle(
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      )
+                    : Text(''),
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/screens/user-menu.dart
+++ lib/screens/user-menu.dart
@@ -4,12 +4,71 @@ class UserMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('User Menu'),
-      ),
-      body: Center(
-        child: Text('Pagina do usuario escolher criar ou entrar em uma sala'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Menu de Salas'),
+          backgroundColor: Colors.orange,
+        ),
+        resizeToAvoidBottomInset: true,
+        body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/Background.png'),
+                    fit: BoxFit.cover,
+                    colorFilter:
+                        ColorFilter.mode(Colors.black45, BlendMode.darken))),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                      margin: EdgeInsets.only(top: 20),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            buttonWidget('Criar Sala'),
+                            buttonWidget('Entrar'),
+                          ])),
+                  salasBoxesWidget(),
+                  salasBoxesWidget(),
+                  salasBoxesWidget(),
+                ])));
   }
+}
+
+Widget buttonWidget(String texto) {
+  return SizedBox(
+      width: 150,
+      child: ElevatedButton(
+          child: Text(
+            texto,
+            style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontFamily: "Calibri",
+                fontWeight: FontWeight.w600),
+          ),
+          style: ElevatedButton.styleFrom(primary: Colors.orange),
+          onPressed: () {}));
+}
+
+Widget salasBoxesWidget() {
+  return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.all(15),
+      width: 370,
+      height: 100,
+      decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5.0))),
+          color: Colors.orange),
+      child: Text("Aqui ficariam as informações de cada sala.",
+          style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontFamily: "Calibri",
+              fontWeight: FontWeight.w600),
+          textAlign: TextAlign.justify));
 }
