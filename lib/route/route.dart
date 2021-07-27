15c81d4a7b7a9cfa58d4ec6cf5b15e0b727df506
Matheus Aragão <matheusaragao@id.uff.br>
2021-07-27 16:52:02 -0300

    Demanda2

--- lib/route/route.dart
+++ lib/route/route.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:grupoazul20211/screens/game.dart';
 import 'package:grupoazul20211/screens/main-menu.dart';
 import 'package:grupoazul20211/screens/user-menu.dart';
 
 //Route Names
 const String mainMenuPage = 'Home';
 const String userMenuPage = 'UserMenu';
+const String gamePage = 'Game';
 
 //Controller
 Route<dynamic> controller(RouteSettings settings) {
   switch (settings.name) {
     case mainMenuPage:
-      return MaterialPageRoute(builder: (context) => MainMenu());
+      return MaterialPageRoute(
+          settings: settings, builder: (context) => MainMenu());
     case userMenuPage:
-      return MaterialPageRoute(builder: (context) => UserMenu());
+      return MaterialPageRoute(
+          settings: settings, builder: (context) => UserMenu());
+    case gamePage:
+      return MaterialPageRoute(
+          settings: settings, builder: (context) => Game());
     default:
       throw ('Rota inexistente');
   }
