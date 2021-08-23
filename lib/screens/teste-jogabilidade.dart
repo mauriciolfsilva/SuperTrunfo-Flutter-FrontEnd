6f015e71890d7c9464342996de1f9bd783ae078e
Henrique Porto <henriqueporto@id.uff.br>
2021-08-22 22:42:50 -0300

    Widget INÍCIO e FIM de partida
    Widget INÍCIO e FIM de partida

--- lib/screens/teste-jogabilidade.dart
+++ lib/screens/teste-jogabilidade.dart
@@ -3,15 +3,29 @@ import 'package:flutter/material.dart';
 import 'package:grupoazul20211/route/route.dart' as router;
 import 'package:grupoazul20211/screens/game.dart';
 import 'package:flip_card/flip_card.dart';
+<<<<<<< Updated upstream
+=======
+import 'dart:async';
+import 'package:show_up_animation/show_up_animation.dart';
+import 'package:grupoazul20211/screens/main-menu.dart';
+>>>>>>> Stashed changes
 
 //classe que extende estados que serão usados para manipular as informações
 //na tela.
+
 class TestePhase extends StatefulWidget {
   @override
   _TestePhaseState createState() => _TestePhaseState();
 }
 
 class _TestePhaseState extends State<TestePhase> {
+<<<<<<< Updated upstream
+=======
+
+  // variavel "inicio" indica que acabamos de iniciar o jogo
+  int inicio = 1;
+
+>>>>>>> Stashed changes
   String p1AnimateState = 'deck';
   String p2AnimateState = 'deck';
 
@@ -47,6 +61,7 @@ class _TestePhaseState extends State<TestePhase> {
         return Alignment.centerRight;
       case 'showCards':
         return Alignment.centerRight;
+<<<<<<< Updated upstream
     }
   }
 
@@ -63,6 +78,24 @@ class _TestePhaseState extends State<TestePhase> {
     }
   }
 
+=======
+    }
+  }
+
+  Alignment p2AnimateAlignment(String state) {
+    switch (state) {
+      case 'deck':
+        return Alignment.topLeft;
+      case 'draw':
+        return Alignment.topCenter;
+      case 'display':
+        return Alignment.centerLeft;
+      case 'showCards':
+        return Alignment.centerLeft;
+    }
+  }
+
+>>>>>>> Stashed changes
   String p1UpdateState(String state) {
     if (state == "draw")
       return "zoom";
@@ -91,8 +124,21 @@ class _TestePhaseState extends State<TestePhase> {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     Carta cartaDaVez =
         Carta(1, "Hélio", 0.0008988, 38, -259.14, 1312.0, 2.2, 7);
+=======
+
+    //chama o metodo mostraWidgetPorUmTempo no inicio
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (inicio == 1) {
+        mostraWidgetPorUmTempo(context: context, widget: vsText());
+        inicio = 0;
+      }
+    });
+
+    Carta cartaDaVez = Carta(1, "Hélio", 0.0008988, 38, -259.14, 1312.0, 2.2, 7);
+>>>>>>> Stashed changes
     return new Scaffold(
         resizeToAvoidBottomInset: true,
         body: SingleChildScrollView(
@@ -108,6 +154,7 @@ class _TestePhaseState extends State<TestePhase> {
           child: Stack(children: [
             Container(
               width: MediaQuery.of(context).size.width,
+<<<<<<< Updated upstream
               height: MediaQuery.of(context).size.height,
               child: AnimatedAlign(
                 alignment: p2AnimateAlignment(p2AnimateState),
@@ -140,6 +187,40 @@ class _TestePhaseState extends State<TestePhase> {
                 alignment: p1AnimateAlignment(p1AnimateState),
                 duration: Duration(seconds: 1),
                 child: AnimatedContainer(
+=======
+              height: MediaQuery.of(context).size.height,
+              child: AnimatedAlign(
+                alignment: p2AnimateAlignment(p2AnimateState),
+                duration: Duration(seconds: 1),
+                child: AnimatedContainer(
+                    width: animateWidth(p2AnimateState) - 10,
+                    height: animateHeight(p2AnimateState) - 15,
+                    margin: EdgeInsets.all(10),
+                    duration: Duration(seconds: 1),
+                    child: FlipCard(
+                        key: cardKey,
+                        flipOnTouch: false,
+                        direction: FlipDirection.HORIZONTAL,
+                        front: Container(
+                            decoration: BoxDecoration(
+                          color: Colors.cyan,
+                          borderRadius: BorderRadius.circular(5),
+                        )),
+                        back: Container(
+                            decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                        )))),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: AnimatedAlign(
+                alignment: p1AnimateAlignment(p1AnimateState),
+                duration: Duration(seconds: 1),
+                child: AnimatedContainer(
+>>>>>>> Stashed changes
                   width: animateWidth(p1AnimateState),
                   height: animateHeight(p1AnimateState),
                   duration: Duration(seconds: 1),
@@ -171,4 +252,133 @@ class _TestePhaseState extends State<TestePhase> {
           ]),
         )));
   }
+}
+
+// Função que permite exibir um widget por um tempo depois remover ele.
+void mostraWidgetPorUmTempo({@required BuildContext context, Widget widget}) {
+  OverlayEntry overlayEntry;
+
+  overlayEntry = OverlayEntry(builder: (context) => widget);
+  Overlay.of(context).insert(overlayEntry);
+  Timer(Duration(seconds: 3), () => overlayEntry.remove());
+}
+
+// Widget que mostra o nome dos dois jogadores no inicio da partida
+Widget vsText() {
+  return Center(
+    child: ShowUpAnimation(
+      //suaviza a entrada pra usar tem q por dependencia no pubspec
+      delayStart: Duration(seconds: 0),
+      animationDuration: Duration(seconds: 1),
+      curve: Curves.bounceIn,
+      direction: Direction.vertical,
+      offset: 0.5,
+      child: Material(
+        type: MaterialType.transparency,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Jogador 1   ', //${this.nome} ?
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 20.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Image.asset(
+              'assets/images/vs.png',
+              width: 50,
+              height: 50,
+              //fit: BoxFit.fill,
+            ),
+            Text(
+              '   Jogador 2', //${this.nome} ?
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 20.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
+
+
+// Widget que mostra a tela de vitória ou derrota no fim da partida
+/* pra testar coloca esse widget:
+
+resultado('Vitória', Colors.green, context),
+
+*/
+Widget resultado(String text, MaterialColor cor, BuildContext context) {
+  return Center(
+    child: ShowUpAnimation(
+      //suaviza a entrada pra usar tem q por dependencia no pubspec
+      delayStart: Duration(seconds: 0),
+      animationDuration: Duration(seconds: 1),
+      curve: Curves.bounceIn,
+      direction: Direction.vertical,
+      offset: 0.5,
+      child: Material(
+        type: MaterialType.transparency,
+        child: Container(
+          height: 200,
+          width: 350,
+          child: Card(
+            shadowColor: Colors.blue,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      text,
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 65.0,
+                        color: cor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Image.asset(
+                      "assets/images/dance-anime-omae-wa-mou-shindeiru.gif",
+                      height: 125.0,
+                      width: 125.0,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  width: 200,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => MainMenu()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                    ),
+                    child: Text(
+                      'Jogar novamente',
+                      style: new TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Montserrat',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
