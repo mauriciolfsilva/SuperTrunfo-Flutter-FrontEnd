581bf5adcd3203cbaf43703885fa7f9205b74f65
Henrique Porto <henriqueporto@id.uff.br>
2021-09-05 23:55:42 -0300

    Adicionando Regras e Icones nos botões
    Adicionando Regras e Icones nos botões

--- lib/screens/main-menu.dart
+++ lib/screens/main-menu.dart
@@ -82,46 +82,174 @@ class _MainMenuState extends State<MainMenu> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        resizeToAvoidBottomInset: true,
-        body: SingleChildScrollView(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            width: double.infinity,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/Background.png'),
-                    fit: BoxFit.cover,
-                    colorFilter:
-                        ColorFilter.mode(Colors.black45, BlendMode.darken))),
-            child: Column(
-              children: <Widget>[
-                Image.asset(
-                  'assets/images/Logo.png',
-                  height: 350.0,
-                ),
-                screenController == 'Home'
-                    ? SizedBox(
-                        width: 200,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              screenController = 'Login';
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.orange,
-                          ),
-                          child: Text(
-                            'Iniciar Jogo',
-                            style: new TextStyle(
-                              fontSize: 20.0,
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/Background.png'),
+                  fit: BoxFit.cover,
+                  colorFilter:
+                      ColorFilter.mode(Colors.black45, BlendMode.darken))),
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                'assets/images/Logo.png',
+                height: 350.0,
+              ),
+              screenController == 'Home'
+                  ? Column(
+                      children: <Widget>[
+                        SizedBox(
+                          width: 200,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                screenController = 'Login';
+                              });
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.blue,
+                            ),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.videogame_asset,
+                                  color: Colors.pink,
+                                  size: 24.0,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Iniciar Jogo',
+                                  style: new TextStyle(
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ),
-                      )
-                    : Text(''),
-                screenController == 'Login' || screenController == 'Cadastrar'
-                    ? SizedBox(
+                        SizedBox(
+                          width: 200,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                screenController = 'Regras';
+                              });
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.orange,
+                            ),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.menu_book,
+                                  color: Colors.pink,
+                                  size: 24.0,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Regras',
+                                  style: new TextStyle(
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  : Column(),
+              screenController == 'Regras'
+                  ? Card(
+                      child: ListTile(
+                        title: RichText(
+                          text: TextSpan(
+                            text: '',
+                            style: TextStyle(color: Colors.black),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: 'Quem começa?\n',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextSpan(
+                                  text: 'O primeiro a entrar na sala.\n\n'),
+                              TextSpan(
+                                text: 'Quando o jogo termina?\n',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextSpan(
+                                  text:
+                                      'Assim que um dos jogadores obter 7 pontos.\n\n'),
+                              TextSpan(
+                                text:
+                                    'Quando um atributo de um elemento ganha do outro?\n',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextSpan(text: 'Quando ele for maior.\n'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      elevation: 8,
+                      shadowColor: Colors.blue,
+                      margin: EdgeInsets.all(20),
+                      shape: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.blue, width: 1)),
+                    )
+                  : Column(),
+              screenController == 'Login' || screenController == 'Cadastrar'
+                  ? SizedBox(
+                      width: 250,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {});
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                        ),
+                        child: TextFormField(
+                          validator: (val) => val.length < 4
+                              ? 'Nome de usuario muito curto.'
+                              : null,
+                          onChanged: (value) {
+                            if (value.length > 0) {
+                              playerName = value;
+                            }
+                          },
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              labelText: 'Digite seu usuario'),
+                        ),
+                      ),
+                    )
+                  : Column(),
+              screenController == 'Login' || screenController == 'Cadastrar'
+                  ? Padding(
+                      padding: const EdgeInsets.only(
+                        left: 40,
+                        top: 20,
+                        right: 40,
+                        bottom: 20,
+                      ),
+                      child: SizedBox(
                         width: 250,
                         child: ElevatedButton(
                           onPressed: () {
@@ -131,128 +259,132 @@ class _MainMenuState extends State<MainMenu> {
                             primary: Colors.white,
                           ),
                           child: TextFormField(
-                            validator: (val) => val.length < 4
-                                ? 'Nome de usuario muito curto.'
-                                : null,
+                            obscureText: _obscureText,
+                            validator: (val) =>
+                                val.length < 4 ? 'Senha muito curta.' : null,
                             onChanged: (value) {
                               if (value.length > 0) {
-                                playerName = value;
+                                playerPassword = value;
                               }
                             },
                             decoration: InputDecoration(
                                 border: InputBorder.none,
-                                labelText: 'Digite seu usuario'),
+                                labelText: 'Digite sua senha'),
                           ),
                         ),
-                      )
-                    : Text(''),
-                screenController == 'Login' || screenController == 'Cadastrar'
-                    ? Padding(
-                        padding: const EdgeInsets.only(
-                          left: 40,
-                          top: 20,
-                          right: 40,
-                          bottom: 20,
+                      ))
+                  : Column(),
+              screenController == 'Login'
+                  ? SizedBox(
+                      width: 200,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            logInUser();
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.deepPurpleAccent,
                         ),
-                        child: SizedBox(
-                          width: 250,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              setState(() {});
-                            },
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.white,
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.login,
+                              color: Colors.pink,
+                              size: 24.0,
                             ),
-                            child: TextFormField(
-                              obscureText: _obscureText,
-                              validator: (val) =>
-                                  val.length < 4 ? 'Senha muito curta.' : null,
-                              onChanged: (value) {
-                                if (value.length > 0) {
-                                  playerPassword = value;
-                                }
-                              },
-                              decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  labelText: 'Digite sua senha'),
+                            SizedBox(
+                              width: 10,
                             ),
-                          ),
-                        ))
-                    : Text(''),
-                screenController == 'Login'
-                    ? SizedBox(
-                        width: 200,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              logInUser();
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.deepPurpleAccent,
-                          ),
-                          child: Text(
-                            'Login',
-                            style: new TextStyle(
-                              fontSize: 20.0,
+                            Text(
+                              'Login',
+                              style: new TextStyle(
+                                fontSize: 20.0,
+                              ),
                             ),
-                          ),
+                          ],
                         ),
-                      )
-                    : Text(''),
-                screenController == 'Home' || screenController == 'Cadastrar'
-                    ? SizedBox(
-                        width: 200,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              if (screenController == 'Cadastrar') {
-                                signInNewUser();
-                              } else {
-                                screenController = 'Cadastrar';
-                              }
-                              // var db = FirebaseFirestore.instance;
-                              // db.collection("usuarios").add({
-                              //   "nome": playerName,
-                              // });
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.green,
-                          ),
-                          child: Text(
-                            'Cadastrar',
-                            style: new TextStyle(
-                              fontSize: 20.0,
-                            ),
-                          ),
+                      ),
+                    )
+                  : Column(),
+              screenController == 'Home' || screenController == 'Cadastrar'
+                  ? SizedBox(
+                      width: 200,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            if (screenController == 'Cadastrar') {
+                              signInNewUser();
+                            } else {
+                              screenController = 'Cadastrar';
+                            }
+                            // var db = FirebaseFirestore.instance;
+                            // db.collection("usuarios").add({
+                            //   "nome": playerName,
+                            // });
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green,
                         ),
-                      )
-                    : Text(''),
-                screenController != 'Home'
-                    ? SizedBox(
-                        width: 200,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              screenController = 'Home';
-                            });
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.redAccent,
-                          ),
-                          child: Text(
-                            'Retornar',
-                            style: new TextStyle(
-                              fontSize: 20.0,
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.assistant_photo,
+                              color: Colors.pink,
+                              size: 24.0,
                             ),
-                          ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Cadastrar',
+                              style: new TextStyle(
+                                fontSize: 20.0,
+                              ),
+                            ),
+                          ],
                         ),
-                      )
-                    : Text(''),
-              ],
-            ),
+                      ),
+                    )
+                  : Column(),
+              screenController != 'Home'
+                  ? SizedBox(
+                      width: 200,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            screenController = 'Home';
+                          });
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.teal,
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.arrow_back,
+                              color: Colors.pink,
+                              size: 24.0,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Retornar',
+                              style: new TextStyle(
+                                fontSize: 20.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  : Column(),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
